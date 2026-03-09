f0e18545a1291c38e285d0e1a32a7fc62e3e68e9
Alexandra <aabochkova@gmail.com>
2026-03-09 20:43:52 +0300

    Создана структура
    Начальная структура классов на основе UML модельки

---
+++ src/main/java/mission/laba1/missionanalyzer/Laba1MissionAnalyzer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mission.laba1.missionanalyzer;
+
+/**
+ *
+ * @author aleksandra
+ */
+public class Laba1MissionAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
