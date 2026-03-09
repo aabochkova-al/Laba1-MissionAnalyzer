ba1f04c4d4ce2d0d3520f67252aa6a9b306a1a67
Alexandra <aabochkova@gmail.com>
2026-03-09 21:46:08 +0300

    Update Curse.java
    Добавлены необходимые поля и геттеры/сеттеры

--- src/main/java/mission/laba1/missionanalyzer/Curse.java
+++ src/main/java/mission/laba1/missionanalyzer/Curse.java
@@ -9,5 +9,12 @@ package mission.laba1.missionanalyzer;
  * @author aleksandra
  */
 public class Curse {
+    private String name;
+    private String threatLevel;
     
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    
+    public String getThreatLevel() { return threatLevel; }
+    public void setThreatLevel(String threatLevel) { this.threatLevel = threatLevel; }
 }
