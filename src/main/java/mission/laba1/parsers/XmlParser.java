b878a8d58847b6de1b76a071c3e676dd928023b7
Alexandra <aabochkova@gmail.com>
2026-03-10 20:11:11 +0300

    Update XmlParser.java
    Написан xml парсер через Jackson

--- src/main/java/mission/laba1/parsers/XmlParser.java
+++ src/main/java/mission/laba1/parsers/XmlParser.java
@@ -4,10 +4,21 @@
  */
 package mission.laba1.parsers;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.io.File;
+import java.io.IOException;
+import mission.laba1.missionanalyzer.Mission;
+
 /**
  *
  * @author aleksandra
  */
 public class XmlParser implements MissionParser{
+    private XmlMapper mapper = new XmlMapper();
     
+    @Override
+    public Mission parse(String filepath) throws IOException {
+       File file = new File(filepath);
+       return mapper.readValue(file, Mission.class);
+    }
 }
